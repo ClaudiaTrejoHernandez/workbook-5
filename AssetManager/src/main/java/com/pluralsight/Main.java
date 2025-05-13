a033d62cc65307f14738c922a883e023e27c888f
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-13 13:42:38 -0700

    Using instanceOf to display adress and make/model
    -Used instanceOf to loop through the assets to detect the type of asset it is and it prints out the correct information depending on asset type

--- AssetManager/src/main/java/com/pluralsight/Main.java
+++ AssetManager/src/main/java/com/pluralsight/Main.java
@@ -5,22 +5,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Asset> assetList = new ArrayList<Asset>();
+        ArrayList<Asset> assets = new ArrayList<Asset>();
 
-        assetList.add(new House("Regular Home","02-15-2015",75000,"100 Park Street, Eureka, CA 94100",3,1500,3));
-        assetList.add(new House("Vacation Home","05-01-2020",85500,"200 Vacation Lane, Honolulu, HI 96810",1,2000,5));
+        assets.add(new House("Regular Home","02-15-2015",75000,"100 Park Street, Eureka, CA 94100",3,1500,3));
+        assets.add(new House("Vacation Home","05-01-2020",85500,"200 Vacation Lane, Honolulu, HI 96810",1,2000,5));
 
-        assetList.add(new Vehicle("Regular Car","03-03-2023",22000,"Hyundai Elantra",2023,17000));
-        assetList.add(new Vehicle("Vacation Car","01-23-2025",27000,"Toyota Camry",2025,5000));
+        assets.add(new Vehicle("Regular Car","01-02-2024",22000,"Hyundai Elantra",2023,17000));
+        assets.add(new Vehicle("Vacation Car","01-23-2025",27000,"Toyota Camry",2025,5000));
 
-        for (Asset asset : assetList) {
+        for (Asset asset : assets) {
             System.out.println("Description: " + asset.getDescription());
             System.out.println("Acquired On: " + asset.getDateAcquired());
             System.out.printf("Purchase Price: $%.2f%n", asset.getOriginalCost());
             System.out.printf("Current Value: $%.2f%n", asset.getValue());
+
+            if (asset instanceof House) {
+                House house = (House) asset;
+                System.out.println("Address: " + house.getAddress());
+            }
+            if (asset instanceof Vehicle) {
+                Vehicle vehicle = (Vehicle) asset;
+                System.out.println("Make & Model: " + vehicle.getMakeModel());
+            }
             System.out.println("-----------------------------------");
+
         }
-
-
+        
     }
 }
